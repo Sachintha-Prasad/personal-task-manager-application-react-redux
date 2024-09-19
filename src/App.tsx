af79a1ec4c61cf5c6a9bd38c5b22e7348c3a6734
Sachintha Prasad <prasadsachintha1231@gmail.com>
2024-09-19 15:21:04 +0530

    feat(app): create a layout and add task table , drawer and other components

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import DrawerToogleBtn from "./features/drawer/DrawerToogleBtn"
+import AddTask from "./components/AddTask"
+import TaskList from "./components/TaskList"
+import { Col, Flex, Layout, Typography } from "antd"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <Layout style={{ minHeight: "100vh" }}>
+            <Layout.Header
+                style={{ padding: 0, display: "flex", alignItems: "center" }}
+            >
+                <Col xs={{ span: 20, offset: 2 }}>
+                    <Flex align="center" justify="space-between">
+                        <Typography.Text
+                            style={{
+                                color: "white",
+                                fontSize: 18,
+                                fontWeight: 500,
+                                margin: 0
+                            }}
+                        >
+                            Personal Task Manager Application
+                        </Typography.Text>
+                        <DrawerToogleBtn type="primary">
+                            Add Task
+                        </DrawerToogleBtn>
+                    </Flex>
+                </Col>
+            </Layout.Header>
+            <Layout.Content>
+                <Col xs={{ span: 20, offset: 2 }} style={{ marginTop: 48 }}>
+                    <TaskList />
+                </Col>
+            </Layout.Content>
+
+            {/* add task drawer */}
+            <AddTask />
+        </Layout>
+    )
 }
 
-export default App;
+export default App
